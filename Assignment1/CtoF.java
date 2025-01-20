570dd4502328a322768c397f8ecf2d1be6076b96
Rashi123ab <rashia729@gmail.com>
2025-01-20 12:07:53 +0530

    Delete Assignment1 directory

--- Assignment1/CtoF.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-class CtoF{
-	public static void main(String[]args){
-		Scanner sc=new Scanner(System.in);
-		int C=sc.nextInt();
-		int F =(C*9/5)+32;
-		System.out.println(F);
-	}
-}
