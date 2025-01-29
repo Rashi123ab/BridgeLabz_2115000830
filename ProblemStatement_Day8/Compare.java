3076b93ab1e449f7cd540dd000a851975ad4d915
Rashi123ab <rashia729@gmail.com>
2025-01-29 15:11:03 +0530

    Assignment 3

---
+++ ProblemStatement_Day8/Compare.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class Compare{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter String 1: ");
+        String s1 = sc.nextLine();
+        System.out.print("Enter String 2: ");
+        String s2 = sc.nextLine();
+        int res=compareLexicographically(s1, s2);
+        if(res<0){
+            System.out.println(s1 + " comes before " + s2 + " in lexicographical order.");
+        }else if(res>0){
+            System.out.println(s1 + " comes after " + s2 + " in lexicographical order.");
+        }else{
+            System.out.println(s1 + " is equal to " + s2 + " lexicographically.");
+        }
+    }
+    public static int compareLexicographically(String s1,String s2) {
+        int length1=s1.length();
+        int length2=s2.length();
+        int minLength=Math.min(length1,length2);
+        for(int i=0;i<minLength;i++){
+            if(s1.charAt(i)!=s2.charAt(i)){
+                return s1.charAt(i)-s2.charAt(i);
+            }
+        }
+        return length1-length2;
+    }
+}
---
+++ ProblemStatement_Day8/ReverseString.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class ReverseString{
+     public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Input string:");
+        String s=sc.next();
+        char [] charArray=s.toCharArray();
+        int i=0, j=s.length()-1;
+        while(i<j){
+           char temp=charArray[i];
+           charArray[i]=charArray[j];
+           charArray[j]=temp;
+           i++;
+           j--;
+        }
+        String ans=new String(charArray);
+        System.out.println("Reverse string is: "+ans);
+     }
+ }
+
